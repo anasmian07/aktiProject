f5340f2ad825c8c841d21fefdde21e131af32645
Your Name <you@example.com>
2024-05-13 17:10:21 +0500

    java

--- maGroup/my.js
+++ maGroup/my.js
@@ -4,4 +4,15 @@ let result=14%2
 console.log(result)
 const add=document.getElementById("add")
 add.innerHTML='10'
+let age = 25; 
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are not yet an adult.");
+}
+const age=document.getElementById("age")
+age.innerHTML='You are not an adult'
+
+       
 
